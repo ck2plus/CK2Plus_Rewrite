e69fbcfbb105a0d497dd8085673c1ac046cb5265
LordPeter <lord.peter.ii@gmx.de>
2019-09-15 19:03:24 +0200

    ADD: Baby portraits & baby trait
    Baby portraits for 0-2 year old with game rule. Baby trait with malus regardless of game rule.
    Known issues: Gender icon not visible on small character frames (e.g. character finder). Graphical problem, need to redraw.
    Vanilla issue/bandaid: babies generated via the vanilla game rule event will not have proper portraits/traits until 1 day after gamestart. no better fix possible.
    Vanilla issue: Babies generated via event elsewhere will not have proper portraits/event until reload. since it's not possible to scope to all newly generated characters, no easy fix that does not involve checking every vanilla event file. again, loading save will fix it, which is good enough for now.

---
+++ CK3/common/defines/000_CK2Plus_defines.lua
@@ -0,0 +1,6 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.DEMESNE_BASE_MAX_SIZE = 2.0
+
+-- needed for baby portraits:
+NDefines.NCharacter.AGE_CHILDHOOD_PULSE = 2                            -- The childhood pulse events start from this age 
